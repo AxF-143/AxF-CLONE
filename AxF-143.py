f15319588e31ed16e2c904a1699981369ea1b56c
AxF-143 <ahmedashraful12@outlook.com>
2024-08-25 14:18:25 +0600

    Update AxF-143.py

--- AxF-143.py
+++ AxF-143.py
@@ -5,6 +5,6 @@ print('\033[92;1m Follow My Facebook'💚)
 os.system('xdg-open https://m.me/ARIFIN.RAJ00')
 fbd=platform.architecture()[0]
 if fbd=="32bit":
-    __import__("ALFE-GREEN")
+    __import__("GREEN")
 elif fbd=="64bit":
-    __import__("ALFE-GREEN")
+    __import__("GREEN")
