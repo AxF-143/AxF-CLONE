ff7f1e7a680d0d3ce3f5e7ee970e1f613b99fcea
AxF-143 <ahmedashraful12@outlook.com>
2024-08-28 23:43:23 +0600

    Update AxF-143.py

--- AxF-143.py
+++ AxF-143.py
@@ -1,10 +1,1 @@
-import os,platform
-os.system('git pull')
-os.system("clear")
-print('\033[92;1m Follow My Facebook')
-os.system('xdg-open https://m.me/ARIFIN.RAJ00')
-fbd=platform.architecture()[0]
-if fbd=="32bit":
-    __import__("X")
-elif fbd=="64bit":
-    __import__("X")
+import X
